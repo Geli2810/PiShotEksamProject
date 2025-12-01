3e2fa5826865241b5eb82a09d6687b67706c1425
EmlShield <eml004@edu.zealand.dk>
2025-12-01 10:26:59 +0100

    Fix

--- PiShotFrontend/index.js
+++ PiShotFrontend/index.js
@@ -85,12 +85,3 @@ createApp({
         }
     }
 }).mount('#app');
-Vue.createApp({
-    data() {
-        return {
-            profiles: [],
-            loading: true,
-            error: null,
-            apiBaseUrl: https://pishot-project-hqd6ffa0gvejbufu.canadacentral-01.azurewebsites.net/api/Profile
-        }
-    }
