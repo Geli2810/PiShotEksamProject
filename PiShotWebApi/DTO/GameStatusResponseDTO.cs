6aeb7ce35ff00ef49909a580e72aaa7ec3838975
Vivor1212 <syhlerp@yahoo.dk>
2025-12-08 14:36:55 +0100

    GetCurrentStatus

--- PiShotWebApi/DTO/GameStatusResponseDTO.cs
+++ PiShotWebApi/DTO/GameStatusResponseDTO.cs
@@ -2,7 +2,7 @@
 {
     public class GameStatusResponseDTO
     {
-        private readonly PiShotProject.Interfaces.IGameRepository _repository;
+        //private readonly PiShotProject.Interfaces.IGameRepository _repository;
 
         public bool IsActive { get; set; }
         public int P1_Id { get; set; }
@@ -12,19 +12,19 @@
         public string? WinnerName { get; set; }
         public string? WinnerImage { get; set; }
 
-        public GameStatusResponseDTO GetCurrentStatus()
-        {
-            var game = _repository.GetState();
+        //public GameStatusResponseDTO GetCurrentStatus()
+        //{
+        //    var game = _repository.GetState();
 
-            return new GameStatusResponseDTO
-            {
-                IsActive = game?.IsActive ?? false,
-                P1_Id = game?.Player1Id ?? 0,
-                P2_Id = game?.Player2Id ?? 0,
-                CurrentWinnerId = game?.CurrentWinnerId,
-                WinnerName = game?.WinnerName,
-                WinnerImage = game?.WinnerImage
-            };
-        }
+        //    return new GameStatusResponseDTO
+        //    {
+        //        IsActive = game?.IsActive ?? false,
+        //        P1_Id = game?.Player1Id ?? 0,
+        //        P2_Id = game?.Player2Id ?? 0,
+        //        CurrentWinnerId = game?.CurrentWinnerId,
+        //        WinnerName = game?.WinnerName,
+        //        WinnerImage = game?.WinnerImage
+        //    };
+        //}
     }
 }
