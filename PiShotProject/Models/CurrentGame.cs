14936978f82df9ef26d1b6d9e81584ec1dbc773d
Vivor1212 <syhlerp@yahoo.dk>
2025-12-09 12:07:01 +0100

    game er refaktoreret så live virker

--- PiShotProject/Models/CurrentGame.cs
+++ PiShotProject/Models/CurrentGame.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PiShotProject.Models
 {
     public class CurrentGame
     {
         public int Id { get; set; }
+
         public int Player1Id { get; set; }
         public int Player2Id { get; set; }
+
         public bool IsActive { get; set; }
         public DateTime? StartTime { get; set; }
 
@@ -20,11 +18,7 @@ namespace PiShotProject.Models
 
         public int? CurrentWinnerId { get; set; }
 
-        public Profile Player1 { get; set; }
-        public Profile Player2 { get; set; }
-
-        public string WinnerName { get; set; }
-
-
+        public Profile? Player1 { get; set; }
+        public Profile? Player2 { get; set; }
     }
 }
