d5322465365b75cda91559628c3092a342c724f4
Vivor1212 <syhlerp@yahoo.dk>
2025-12-09 14:01:11 +0100

    cors ændret

--- PiShotWebApi/Program.cs
+++ PiShotWebApi/Program.cs
@@ -30,17 +30,6 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("SpecificCorsPolicy", policy =>
-    {
-        // ERSTATTET: AllowAnyOrigin()
-        policy.WithOrigins("https://white-cliff-00bf1d610.3.azurestaticapps.net/")
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
-
 
 var cs = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<PiShotDBContext>(opts => opts.UseSqlServer(cs));
@@ -50,7 +39,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseCors("SpecifyCorsPolicy");
+app.UseCors("AllowAll");
 app.MapControllers();
 
 app.Run();
