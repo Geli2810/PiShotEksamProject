472fcc8b4f2902620bf5c0fa5b7843569d13a0e3
Vivor1212 <syhlerp@yahoo.dk>
2025-12-09 13:04:25 +0100

    ny program.cs

--- PiShotWebApi/Program.cs
+++ PiShotWebApi/Program.cs
@@ -6,40 +6,36 @@ using PiShotProject.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// --- Services ---
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// CORS (vi registrerer bare CORS, men bruger inline policy senere)
-builder.Services.AddCors();
-
-// DI
 builder.Services.AddScoped<IGameRepository, GameRepository>();
 builder.Services.AddScoped<IGameService, GameService>();
 builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
 builder.Services.AddScoped<IScoreRepository, ScoreRepository>();
 
-// DB
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<PiShotDBContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<PiShotDBContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
+
 app.UseSwagger();
+
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-
-// Routing -> CORS -> Auth -> Controllers
-app.UseRouting();
-
-// *** VIGTIGT: global CORS-policy inline ***
-app.UseCors(cors => cors
-    .AllowAnyOrigin()
-    .AllowAnyMethod()
-    .AllowAnyHeader()
-);
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
